f902860a34a17e687092d5c7cf8b194998a87cfa
ybrun <ybrun@dmic-corp.fr>
2017-01-15 14:48:15 +0100

    Mise à jour du numéro de version dans le pied de page

--- front/inc/footer/footer_users.inc.php
+++ front/inc/footer/footer_users.inc.php
@@ -13,6 +13,6 @@
     <td scope="row"><div align="center">Suivez nous sur <a href="https://www.facebook.com/people/Dmic-Corp/100011409148719" target="_blank"><img src="http://www.dmic-corp.fr/images/facebook.jpg" border="0" alt="Facebook" /></a>
     <a href="https://fr.linkedin.com/in/dmic-corp-920961118" target="_blank"><img src="http://www.dmic-corp.fr/images/linkedin.jpg" border="0" alt="Linked'In" /></a>
     <a href="http://fr.viadeo.com/fr/profile/dmic.corp" target="_blank"><img src="http://www.dmic-corp.fr/images/viadeo.jpg" border="0" alt="Viadeo" /></a></div></td>
-    <td scope="row"><div align="center">Version 3.0</div></td>
+    <td scope="row"><div align="center">Version 3.5</div></td>
   </tr>
 </table>
